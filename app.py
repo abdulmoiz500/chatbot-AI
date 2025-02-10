5b7e5183b333683b951267c161ed845ef4d0421d
ABDUL MOIZ <abdulmoizghani500@gmail.com>
2025-02-10 14:27:36 -0800

    Fixed dependency versions

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS 
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
